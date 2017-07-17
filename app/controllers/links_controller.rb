d700ad1629943a75efc57090a3532bd1ab592535
jk1dd <portajonk@gmail.com>
2017-07-17 14:26:31 -0600

    allow user to submit and view links, non jQuery

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,8 +5,27 @@ class LinksController < ApplicationController
       redirect_to login_path
     else
       @link = Link.new
-      @links = Link.all
+      @links = current_user.links.all
     end
   end
 
+  def create
+    user = current_user
+    # binding.pry
+    link = user.links.new(link_params)
+    if link.save
+      flash[:success] = "Link created successfully"
+      # render root_path
+      redirect_to root_path
+    else
+      flash[:notice] = 'Link not created'
+      # render root_path
+      redirect_to root_path
+    end
+  end
+
+  private
+  def link_params
+    params.require(:link).permit(:url, :title)
+  end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,5 @@
 class Link < ActiveRecord::Base
-
+  validates :url, presence: true, :url => true
+  validates :title, presence: true
+  belongs_to :user
 end

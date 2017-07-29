387b8ce4f5b3c937031e07228ff9df0b2ce6ae48
jk1dd <portajonk@gmail.com>
2017-07-28 18:49:52 -0600

    model tests passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
-# require 'rails_helper'
-#
-# RSpec.describe User, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "attributes" do
+    user = User.create(email: "a@a.com", password: "123", password_confirmation: "123")
+    expect(user).to respond_to(:email)
+    expect(user).to respond_to(:password_confirmation)
+  end
+
+  it "relationships" do
+    user = User.create(email: "a@a.com", password: "123", password_confirmation: "123")
+    expect(user).to respond_to(:links)
+  end
+
+
+end
